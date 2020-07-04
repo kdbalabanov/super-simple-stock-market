d697a6b437c9b4507f278d700f044e78535522d2
kdbalabanov <k.d.balabanov@gmail.com>
2020-07-04 02:27:26 +0100

    Created SuperSimpleStockMarket project and main class

---
+++ SuperSimpleStockMarket/src/main/java/com/company/stockmarket/service/StockMarketService.java
@@ -0,0 +1,7 @@
+package main.java.com.company.stockmarket.service;
+
+public class StockMarketService {
+    public static void main(String[] args) {
+        System.out.println("Hello from Stock Market Service");
+    }
+}
